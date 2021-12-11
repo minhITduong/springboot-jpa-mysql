8798f07272aff8a3591f052f773f8eb1ea62f618
Minh-Chien <mchien.duong@gmail.com>
2021-12-11 22:11:26 +0100

    commit

---
+++ src/test/java/com/duong/springbootjpamysql/SpringbootJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duong.springbootjpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
